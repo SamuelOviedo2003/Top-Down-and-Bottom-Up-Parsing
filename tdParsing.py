198b998fdcb580a67d3bb0eb70b4c58b919be4d0
SamuelOviedo2003 <92952464+SamuelOviedo2003@users.noreply.github.com>
2023-04-20 14:42:33 -0500

    solucion First y Follow

--- tdParsing.py
+++ tdParsing.py
@@ -1,12 +1,16 @@
 import string
 from collections import deque
+import pandas as pd
+from typing import List
 
 
 class TopDownParsing:
     def __init__(self, gramatica):
         self.gramatica = gramatica
+        self.terminales = {}
         self.first = {}
         self.follow = {}
+        self.tabla = pd.DataFrame()
 
     def nT(self):
         pila = list(self.gramatica.keys())
@@ -17,13 +21,15 @@ class TopDownParsing:
                     if cadena != "ε" and (cadena.islower() | cadena.isnumeric() | (cadena in string.punctuation)):
                         if cadena not in self.first:
                             self.first[cadena] = set()
-                        self.first[cadena].add(cadena)
+                            self.terminales[cadena] = set()
+                        self.first[cadena] = cadena
+                        self.terminales[cadena] = cadena
                         if cadena not in self.follow:
                             self.follow[cadena] = set()
                         self.follow[cadena].add("NA")
 
     def calculateFirst(self):
-        dictAux = self.gramatica
+        dictAux = {k: v[:] for k, v in self.gramatica.items()}
         pila = list(dictAux.keys())
         dependiente = []
         faltantes = []
@@ -31,36 +37,49 @@ class TopDownParsing:
             while pila:
                 llave = pila.pop()
                 for valor in dictAux.get(llave):
-                    if (valor[0].islower() | valor[0].isnumeric() | (valor[0] in string.punctuation)):
-                        if llave not in self.first:
-                            self.first[llave] = set()
-                        self.first[llave].add(valor[0])
-                    elif valor[0].isupper():
-                        if(valor[0] in self.first):
+                    if valor != "":
+                        if (valor[0].islower() | valor[0].isnumeric() | (valor[0] in string.punctuation)):
                             if llave not in self.first:
                                 self.first[llave] = set()
-                            self.first[llave].update(self.first.get(valor[0]))
-                        else:
-                            dependiente.insert(0, llave)
-                    if "ε" in valor:
-                        faltantes.append(llave)
+                            self.first[llave].add(valor[0])
+                        elif valor[0].isupper():
+                            if(valor[0] in self.first):
+                                if llave not in self.first:
+                                    self.first[llave] = set()
+                                self.first[llave].update(
+                                    list(filter(lambda v: v != 'ε', self.first.get(valor[0]))))
+                            else:
+                                dependiente.insert(0, llave)
+                        if "ε" in valor:
+                            faltantes.append(llave)
+                    else:
+                        self.first[llave].update("ε")
 
             while(dependiente):
                 llave2 = dependiente.pop()
                 for valor in dictAux.get(llave2):
-                    if(valor[0] in self.first):
-                        if llave not in self.first:
-                            self.first[llave2] = set()
-                        self.first[llave2].update(self.first.get(valor[0]))
+                    if valor:
+                        if(valor[0] in self.first):
+                            if llave2 not in self.first:
+                                self.first[llave2] = set()
+                            self.first[llave2].update(
+                                list(filter(lambda v: v != 'ε', self.first.get(valor[0]))))
+            aux = False
             while(faltantes):
                 llave = faltantes.pop()
                 for key in dictAux.keys():
                     for cadena in dictAux.get(key):
-                        if cadena[0] == llave:
+                        if len(cadena) > 0 and cadena[0] == llave:
                             posicion = dictAux.get(key).index(cadena)
                             dictAux[key][posicion] = dictAux.get(key)[
                                 posicion][1:]
                             pila.append(key)
+                            aux = True
+                        if cadena:
+                            if cadena[0] in dictAux.get(key):
+                                dependiente.append(key)
+                if aux:
+                    break
             if not pila:
                 break
         return self.first
@@ -68,9 +87,14 @@ class TopDownParsing:
     def calculateFollow(self):
         cola = deque(self.gramatica.keys())
         indice = 0
-        visitar = []
+        primero = True
         while cola:
             llave = cola.popleft()
+            if primero == True:
+                if llave not in self.follow:
+                    self.follow[llave] = set()
+                self.follow[llave].add("$")
+                primero = False
             for key in self.gramatica.keys():
                 for cadena in self.gramatica.get(key):
                     try:
@@ -81,51 +105,110 @@ class TopDownParsing:
                             siguiente = ''
                         else:
                             siguiente = cadena[indice+1]
-                        if llave == list(self.gramatica.keys())[0] and (siguiente.islower() | siguiente.isnumeric() | (siguiente in string.punctuation)):
+                        if siguiente != '':
                             if llave not in self.follow:
                                 self.follow[llave] = set()
-                            self.follow[llave].add("$")
-                            continue
-                        if siguiente != '' and 'ε' not in self.first[siguiente]:
-                                if llave not in self.follow:
-                                    self.follow[llave] = set()
-                                self.follow[llave].update(
-                                    self.first.get(siguiente))
-                                continue
-                        
+                            recorrido = deque(cadena[indice+1:])
+                            while(recorrido):
+                                primero1 = recorrido.popleft()
+                                if 'ε' in self.first.get(primero1):
+                                    self.follow[llave].update(
+                                        list(filter(lambda v: v != 'ε', self.first.get(primero1))))
+                                if 'ε' not in self.first.get(primero1):
+                                    self.follow[llave].update(
+                                        list(filter(lambda v: v != 'ε', self.first.get(primero1))))
+                                    break
+
                         if siguiente != '' and 'ε' in self.first[siguiente]:
+                            validacion = False
+                            recorrido1 = deque(cadena[indice+1:])
+                            while(recorrido1):
+                                primero2 = recorrido1.pop()
+                                if 'ε' in self.first[primero2]:
+                                    validacion = True
+                                if 'ε' not in self.first[primero2]:
+                                    validacion = False
+                                    break
                             if key not in self.follow.keys():
                                 cola.append(key)
                                 cola.append(llave)
-                                print("aaaaa")
                             if llave not in self.follow:
                                 self.follow[llave] = set()
-                            self.follow[llave].update(self.first[siguiente] | self.follow[key])
-                            continue
-                    
-                        
+                            if validacion:
+                                self.follow[llave].update(self.follow[key])
                         if siguiente == '':
                             if llave not in self.follow:
                                 self.follow[llave] = set()
-                            self.follow[llave].update(self.follow[key])
-                            continue
+                            if key not in self.follow.keys():
+                                cola.append(key)
+                                cola.append(llave)
+                            if key in self.follow.keys():
+                                self.follow[llave].update(self.follow[key])
+
                     except ValueError:
                         pass
-        print(cola)
         # return indice
 
+    def indicesTabla(self):
+        self.tabla = pd.DataFrame(index=list(
+            self.gramatica.keys()), columns=list(self.terminales.keys()))
+
+    def recursionTabla(self, llave, buscado, produccion: List[str]):
+        producciones = produccion
+        for valor in self.gramatica.get(llave):
+            producciones.append(valor)
+            if(valor[0] != buscado):
+                if valor[0].isupper():
+                    return self.recursionTabla(valor[0], buscado, producciones)
+                else:
+                    producciones.pop()
+            if(valor[0] == buscado):
+                return producciones[0]
+
     def calculateTabla(self):
-        pass
+        # print(len(self.tabla.index))
+        for noTerminal in self.tabla.index:
+            for terminal in self.tabla.columns:
+                if(terminal in self.first[noTerminal]):
+                    self.tabla.at[noTerminal, terminal] = self.recursionTabla(
+                        noTerminal, terminal, [])
+                if('ε' in self.first[noTerminal]):
+                    for valor in self.follow[noTerminal]:
+                        self.tabla.at[noTerminal, valor] = 'ε'
 
     def calculateCondiciones(self):
         pass
 
 
-a = TopDownParsing({"E": ["TA"], "A": ["+TA", "ε"],"T": ["FB"], "B": ["*FB", "ε"], "F": ["(E)", "i"]})
-#a = TopDownParsing({"A": ["BCD"], "B": ["b", "ε"],"C": ["c", "ε"], "D": ["d", "Ce"]})
-#a = TopDownParsing({"S": ["ABC","i","ε"], "A": ["BC", "a"],"B": ["b", "ε"], "C": ["c"]})
+#a = TopDownParsing({"E": ["TA"], "A": ["+TA", "ε"], "T": ["FB"], "B": ["*FB", "ε"], "F": ["(E)", "i"]})
+#a= TopDownParsing({"S":["L=R","R"],"R":["L"],"L":["*R","i"]})
+#a= TopDownParsing({"S":["aaSb","cSb","b"]})
+#a= TopDownParsing({"S":["aSc","B","ε"],"B":["bBc","ε"]})
+#a = TopDownParsing({"A": ["BCD","Aa"], "B": ["b", "ε"],"C": ["c", "ε"], "D": ["d", "Ce"]})
+# a = TopDownParsing({"S": ["ABC","i","ε"], "A": ["BC", "a"],"B": ["b", "ε"], "C": ["c"]})#first
+#a= TopDownParsing({"S" :["aSb", "c"]})
+#a = TopDownParsing({"R": ["EA"], "A": ["EA", "ε"],"E": ["CB"], "B": ["CB", "ε"],"C":["L","(R)"],"L":["a","b","c"]})
+# a = TopDownParsing({"E": ["E+T","E-T","T"], "T": ["T*F", "T/F","F"],"F": ["(E)","n"]})# problema tabla
+
+'''
+a.nT()
 a.calculateFirst()
-# print(a.nT())
-# print(a.first)
-print(a.calculateFollow())
-print(a.follow)
+
+print("First:")
+for clave, valores in a.first.items():
+        print(f"{clave} : {valores}")
+        print("")
+a.calculateFollow()
+
+print("Follow:")
+for clave, valores in a.follow.items():
+        print(f"{clave} : {valores}")
+        print("")
+
+a.indicesTabla()
+a.calculateTabla()
+#print(a.first)
+# print(a.terminales)
+print(a.tabla)
+# print(a.gramatica)'''
+
